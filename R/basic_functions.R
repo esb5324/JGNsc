030a161d393602a13c4c49fe5d0b687557dfbf57
Meichen Dong <meichen@live.unc.edu>
2021-02-23 00:52:11 -0500

    update vignettes

--- R/basic_functions.R
+++ R/basic_functions.R
@@ -3,7 +3,7 @@
 # ---------------------------
 
 #' truncate the (precision) matrix
-#' 
+#'
 #' @param z a matrix
 #' @param threshold the threshold value, where if a value's absolute value is less than this, it will be truncated to zero.
 #' @return a truncated matrix
@@ -14,11 +14,11 @@ trunc_precision <- function(z, threshold=0.01){
 }
 
 #' Transform the precision matrix to partial correlation matrix
-#'  
+#'
 #' @param mat the precision matrix
 #' @return a partial correlation matrix
 #' @export
-prec2partialcorr <- function(mat){ 
+prec2partialcorr <- function(mat){
   denom <- diag(mat)
   n <- ncol(mat)
   out <- mat
@@ -32,18 +32,18 @@ prec2partialcorr <- function(mat){
 }
 
 #' create list of matrices according to conditions
-#' 
+#'
 #' @param mtx raw count matrix, genes by samples
 #' @param group vector of group identities. For example: c("A","A","B","C")
 #' @param min.cells integer, keep genes with at least __ cells expressed. Default is 10.
 #' @param geneSet a vector of genes of interest
 #' @return a list of matrices by group
 #' @export
-getObservedList <- function(mtx, group = NULL, min.cells = 10, geneSet = NULL){ 
+getObservedList <- function(mtx, group = NULL, min.cells = 10, geneSet = NULL){
   if (is.null(geneSet)){
     message("Screening all available genes /n Please make sure group variable matches the samples ID")
     geneSet = rownames(mtx)
-  }  
+  }
   if (is.null(group)){
     group = rep("OneGroup", ncol(mtx))
   }
@@ -86,13 +86,13 @@ getCondRelated <- function(gs, pcor0 = 0.0001, partcorr.list, conditions = NULL)
 #' @param VARPATH the variable name for pathways
 #' @return a table showing the frequencies of genes in each pathway
 #' @export
-#' 
+#'
 getPathFreq <- function(meta, VARPATH ="SigmaMiniMap.Term"){
   nn <- nrow(meta)
   paths <- str_trim(unlist(strsplit(meta[,VARPATH], ";")), side = "both")
   paths <- paths[paths !=""]
   paths.count <- count(paths)
-  paths.count$pct <- paths.count$freq / nn 
+  paths.count$pct <- paths.count$freq / nn
   return(paths.count)
 }
 
@@ -103,7 +103,7 @@ getPathFreq <- function(meta, VARPATH ="SigmaMiniMap.Term"){
 #' @return A plot
 #' @export
 getGENEbox <- function(countmat, group, gene){
-  data = data.frame(Gene = countmat[gene,], 
+  data = data.frame(Gene = countmat[gene,],
                     group = group)
   datamelt <- melt(data)
   ggplot(datamelt, aes(x=group, y=value)) + geom_jitter(alpha = 0.3, color = "lightblue")+ geom_boxplot(alpha = 0.5) +
@@ -117,8 +117,8 @@ getGENEbox <- function(countmat, group, gene){
 #' @param xlabel.pos the position of samplesize/expression rate information
 #' @return a plot
 #' @export
-getGENEhist <- function(countmat, group, gene, xlabel.pos = 500){ 
-  data <- data.frame(group = group, 
+getGENEhist <- function(countmat, group, gene, xlabel.pos = 500){
+  data <- data.frame(group = group,
                      GENEbin = countmat[gene,] > 0,
                      GENEraw = countmat[gene,])
   groupn = table(data$group)
@@ -146,19 +146,19 @@ getGENEhist <- function(countmat, group, gene, xlabel.pos = 500){
 #' @export
 Map2Pathways <- function(partcorr.list, conditions = NULL, GeneInterest = NULL,
                          pathwayRef, pathwayRef_geneVariable, pathwayRef_pathVariable,
-                         threshold =0){ 
+                         threshold =0){
   if (!is.data.frame(pathwayRef)){
     pathwayRef <- as.data.frame(pathwayRef)
   }
   relatedGenes <- getCondRelated(gs=GeneInterest, partcorr.list = partcorr.list, pcor0 = threshold, conditions = conditions)
   x.connect <- pathwayRef[toupper(pathwayRef[,pathwayRef_geneVariable]) %in% rownames(relatedGenes),]
-  
-  # constant 
+
+  # constant
   x.connect <- cbind.data.frame(x.connect, relatedGenes[toupper(x.connect[,pathwayRef_geneVariable]),])
   x.connect[,pathwayRef_pathVariable][x.connect[,pathwayRef_pathVariable] ==""] <- "Undefined;"
   x.allgenes <- pathwayRef[toupper(pathwayRef[,pathwayRef_geneVariable]) %in% toupper(rownames(partcorr.list[[1]])),]
   x.allgenes[,pathwayRef_pathVariable][x.allgenes[,pathwayRef_pathVariable] ==""] <- "Undefined;"
-  
+
   cond.connect <- list()
   cond.freqs <- list()
   cond.vars <- gsub(" ", "_", conditions)
@@ -167,7 +167,7 @@ Map2Pathways <- function(partcorr.list, conditions = NULL, GeneInterest = NULL,
     cond.freqs[[m]] <- getPathFreq(cond.connect[[m]], VARPATH =pathwayRef_pathVariable)
   }
   all.freqs <- getPathFreq(x.allgenes, VARPATH =pathwayRef_pathVariable)
-  
+
   # merge these proportions
   meta.merge <- all.freqs
   for (i in 1:length(cond.freqs)){
@@ -179,7 +179,7 @@ Map2Pathways <- function(partcorr.list, conditions = NULL, GeneInterest = NULL,
   meta.merge[is.na(meta.merge)] <- 0
   nconnect = c(nrow(x.allgenes), unlist(lapply(cond.connect, nrow)))
   names(nconnect) <- c("All", cond.vars)
-  
+
   getFisherPval <- function(subg = 1){
     ft.table <- NULL
     for(ii in 1:nrow(all.freqs)){
@@ -202,13 +202,50 @@ Map2Pathways <- function(partcorr.list, conditions = NULL, GeneInterest = NULL,
     ft.table$Fisher_pval <- as.numeric(ft.table$Fisher_pval)
     return(ft.table)
   }
-  
+
   test.fisher <- lapply(1:length(conditions), getFisherPval)
   # all(test.fisher[[1]]$x ==test.fisher[[2]]$x)
   temp = do.call(cbind, test.fisher)[,- (2*(1:length(conditions)))]
   colnames(temp) <- paste("Fisher_pval",cond.vars, sep = "_")
-  
+
   GSEA.table = cbind(meta.merge, temp)
   return(list(x.connect = x.connect,
+              cond.connect = cond.connect,
               GSEA.table = GSEA.table))
+}
+
+#' plot one network
+#'
+#' @param parcorr partial correlation matrix
+#' @param mode ggnet2 mode, could be "circle" or "spring",...
+#' @param gname character, graph name.
+#' @param lsize numeric, label size
+#' @param nodecolor vector/character, specify the color for each node
+#' @param family.vec vector, assign nodes to families. In other words, it could be the classification / covariates of the nodes.
+#' @param circlenet logical, whether plot the network in a circle shape.
+#' @import ggnet2
+#' @return a network figure
+#' @export
+plot_onenet <- function(parcorr, mode = "circle",gname="", lsize=2.5,
+                        nodecolor="grey", family.vec = 0, circlenet = F){
+  # mode = "spring" /
+  net1 <- network(parcorr, ignore.eval = F, names.eval="weights")
+  set.edge.attribute(net1, "size", abs(net1 %e% "weights")*5)
+  set.edge.attribute(net1, "color", ifelse(net1 %e% "weights"> 0, "red","green"))
+  net1 %v% "family" <- family.vec
+  net1 %v% "importance"  <- abs(rowSums(abs(parcorr)))*2
+  net1 %v% "nconnect"  <- abs(rowSums(abs(parcorr)>0))/10
+  if (circlenet){
+    ti <- ggnet2(net1, label = T, mode = mode, edge.size = "size",
+                 edge.color = "color", node.size = "nconnect", label.size = lsize,
+                 color = nodecolor, legend.position = "none") + ggtitle(gname) #+ theme(legend.position = "none")
+  } else {
+    ti = ggplot(net1, aes(x=x, y=y, xend=xend, yend=yend)) +
+      geom_edges(color = "grey70") +
+      geom_nodes(aes(color = family, size = importance)) +
+      geom_nodetext(aes(label = vertex.names),
+                    fontface = "bold", size = 2.5) +
+      theme_blank( )+ theme(legend.position = "none") + ggtitle(gname)
+  }
+  return(ti)
 }
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # JGNsc
- 
+
+<!-- badges: start -->
+[![Travis build status](https://travis-ci.com/meichendong/JGNsc.svg?token=csxDR5yj8mozKtxdzYCW&branch=main)](https://travis-ci.com/meichendong/JGNsc.svg?token=csxDR5yj8mozKtxdzYCW&branch=main)
+[![CRAN status](https://www.r-pkg.org/badges/version/JGNsc)](https://CRAN.R-project.org/package=JGNsc)
+<!-- badges: end -->
+
+Joint Gene Network Construction by Single Cell RNA Sequencing Data under Multiple Conditions
+
+
+## Cite our work:
+Pending.
+
+## Installation
+The development version from [GitHub](https://github.com/):
+```{r}
+if (!require("devtools")) {
+  install.packages("devtools")
+}
+devtools::install_github("meichendong/JGNsc")
+```
+
+The manuscript is in preparation.
+
+
+## Example
+Detailed tutorial is available at xxxxx.
+```{r example}
+library(JGNsc)
+## basic example code
+```
+
+## FAQs / Notes
+FAQs and solutions will be posted here.
+
+#### Please forward questions/comments to email: `meichen@live.unc.edu`
+
+Thank you!
