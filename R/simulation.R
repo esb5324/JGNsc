5a004fa38807337ca17b14670e0dc4e9b0694886
esb5324 <56692193+esb5324@users.noreply.github.com>
2023-06-23 09:39:30 -0700

    Update simulation.R
    troubleshooting for error "can't find t_net"

--- R/simulation.R
+++ R/simulation.R
@@ -28,6 +28,7 @@ crt_mat_A1 <- function(A0,U=0){
 #' @export
 generateBlki <- function(ni, ud= c(-100:-60, 60:100)/100, runif_threshold, t_net, pd){
   mati <- matrix(0, ni, ni)
+  print("t_net from generateBlki"); print(t_net)
   if (t_net=="random"){
   for (i in 1:ni){
     for (j in i:ni){
@@ -122,6 +123,7 @@ generateSigmaList <- function(nivec.list, ud = c(-100:-60, 60:100)/100,
       nblk <- length(nivec.list[[1]])
       for (b in 1:nblk){
         ni <- nivec.list[[1]][b]
+         print("true_net from generateSigmaList"); print(true_net)
         blklist[[b]] <- generateBlki(ni=ni, ud=ud,runif_threshold=blk_runif_threshold,t_net=true_net,pd=pos_def)
         zeroleft <- matrix(0, nrow = ni, ncol = sum(nivec.list[[1]][0:(b-1)]))
         zeroright <- matrix(0, nrow = ni, ncol = ifelse(b<nblk,sum(nivec.list[[1]][(b+1):nblk]),0))
